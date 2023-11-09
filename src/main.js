8250c9284b2108f6159214b4519cd5c57000ca4b
Fawaz Ahmed <fawazahmed0@hotmail.com>
2023-11-09 07:53:13 +0530

    add

--- src/main.js
+++ src/main.js
@@ -1,8 +1,8 @@
 const core = require('@actions/core')
-const exec = require('@actions/exec');
-const fs = require('node:fs/promises');
-const os = require('node:os'); 
-const path = require('node:path'); 
+const exec = require('@actions/exec')
+const fs = require('node:fs/promises')
+const os = require('node:os')
+const path = require('node:path')
 
 /**
  * The main function for the action.
@@ -11,39 +11,46 @@ const path = require('node:path');
 async function run() {
   try {
     const cred = core.getInput('credentials')
-    await exec.exec('node', ['index.js', 'foo=bar']);
+    await exec.exec('node', ['index.js', 'foo=bar'])
     let credOption = ''
 
-    if(cred)
-    credOption = `-c ${cred}`
+    if (cred) credOption = `-c ${cred}`
 
-    if(os.type() == 'Darwin'){
+    if (os.type() == 'Darwin') {
       await exec.exec('brew install ttyd cloudflare/cloudflare/cloudflared')
       await exec.exec(`ttyd -p 8391 -a -W ${credOption} bash &`)
       await exec.exec('cloudflared tunnel --url http://localhost:8391 &')
-    }else if(os.type() == 'Linux'){
-      await exec.exec('wget -O ttyd https://github.com/tsl0922/ttyd/releases/download/1.7.4/ttyd.aarch64') 
-      await exec.exec('wget -O cloudflared https://github.com/cloudflare/cloudflared/releases/latest/download/cloudflared-linux-amd64') 
+    } else if (os.type() == 'Linux') {
+      await exec.exec(
+        'wget -O ttyd https://github.com/tsl0922/ttyd/releases/download/1.7.4/ttyd.aarch64'
+      )
+      await exec.exec(
+        'wget -O cloudflared https://github.com/cloudflare/cloudflared/releases/latest/download/cloudflared-linux-amd64'
+      )
       await exec.exec('chmod 777 ttyd')
       await exec.exec('chmod 777 cloudflared')
       await exec.exec(`./ttyd -p 8391 -a -W ${credOption} bash &`)
       await exec.exec('./cloudflared tunnel --url http://localhost:8391 &')
-    }else if(os.type() == 'Windows_NT'){
-      await exec.exec('wget -O ttyd.exe https://github.com/tsl0922/ttyd/releases/download/1.7.3/ttyd.win32.exe')
-      await exec.exec('wget -O cloudflared.exe https://github.com/cloudflare/cloudflared/releases/latest/download/cloudflared-windows-amd64.exe')
+    } else if (os.type() == 'Windows_NT') {
+      await exec.exec(
+        'wget -O ttyd.exe https://github.com/tsl0922/ttyd/releases/download/1.7.3/ttyd.win32.exe'
+      )
+      await exec.exec(
+        'wget -O cloudflared.exe https://github.com/cloudflare/cloudflared/releases/latest/download/cloudflared-windows-amd64.exe'
+      )
       await exec.exec(`./ttyd.exe -p 8391 -a -W ${credOption} bash &`)
       await exec.exec('./cloudflared.exe tunnel --url http://localhost:8391 &')
     }
 
     let continuePath1 = path.join(__dirname, 'continue')
-    let continuePath2 = path.join(process.env.GITHUB_WORKSPACE, "continue")
+    let continuePath2 = path.join(process.env.GITHUB_WORKSPACE, 'continue')
 
-    while( !(await fileExists( continuePath1 )) && !(await fileExists( continuePath2 )) ){
-     await sleep('1000')
+    while (
+      !(await fileExists(continuePath1)) &&
+      !(await fileExists(continuePath2))
+    ) {
+      await sleep('1000')
     }
-
-
-
   } catch (error) {
     // Fail the workflow run if an error occurs
     core.setFailed(error.message)
@@ -52,15 +59,15 @@ async function run() {
 
 async function fileExists(pathToFile) {
   try {
-      await fs.access(pathToFile, fs.constants.F_OK)
-      return true
+    await fs.access(pathToFile, fs.constants.F_OK)
+    return true
   } catch (e) {
-      return false
+    return false
   }
 }
 
 function sleep(ms) {
-  return new Promise(resolve => setTimeout(resolve, ms));
+  return new Promise(resolve => setTimeout(resolve, ms))
 }
 
 module.exports = {
