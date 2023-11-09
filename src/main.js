b1a917ca593546038d7872df2220a922a8ac7e4c
Fawaz Ahmed <fawazahmed0@hotmail.com>
2023-11-09 08:27:25 +0530

    add

--- src/main.js
+++ src/main.js
@@ -18,9 +18,7 @@ async function run() {
     if (os.type() == 'Darwin') {
       await exec.exec('brew install ttyd cloudflare/cloudflare/cloudflared')
       exec.exec(`ttyd -p 8391 ${credOption} -a -W bash &`)
-      exec.exec(
-        'cloudflared tunnel --url http://localhost:8391 > mylog.txt 2>&1 &'
-      )
+      exec.exec('cloudflared tunnel --url http://localhost:8391')
     } else if (os.type() == 'Linux') {
       await exec.exec(
         'wget -O ttyd https://github.com/tsl0922/ttyd/releases/download/1.7.4/ttyd.aarch64'
@@ -31,9 +29,7 @@ async function run() {
       await exec.exec('chmod 777 ttyd')
       await exec.exec('chmod 777 cloudflared')
       exec.exec(`./ttyd -p 8391 -a -W ${credOption} bash &`)
-      exec.exec(
-        './cloudflared tunnel --url http://localhost:8391 > mylog.txt 2>&1 &'
-      )
+      exec.exec('./cloudflared tunnel --url http://localhost:8391')
     } else if (os.type() == 'Windows_NT') {
       await exec.exec(
         'wget -O ttyd.exe https://github.com/tsl0922/ttyd/releases/download/1.7.3/ttyd.win32.exe'
@@ -42,21 +38,18 @@ async function run() {
         'wget -O cloudflared.exe https://github.com/cloudflare/cloudflared/releases/latest/download/cloudflared-windows-amd64.exe'
       )
       exec.exec(`./ttyd.exe -p 8391 -a -W ${credOption} bash &`)
-      exec.exec(
-        './cloudflared.exe tunnel --url http://localhost:8391  > mylog.txt 2>&1 &'
-      )
+      exec.exec('./cloudflared.exe tunnel --url http://localhost:8391')
     }
 
     let continuePath1 = path.join(__dirname, 'continue')
     let continuePath2 = path.join(process.env.GITHUB_WORKSPACE, 'continue')
     await sleep(30_000)
 
-    let logfile = path.join(process.env.GITHUB_WORKSPACE, 'mylog.txt')
-    let content = await fs.readFile(logfile, { encoding: 'utf8' })
+    /*
     let url = Array.from(
       content.matchAll(/-{10,}.*?(?<url>https?:\/\/.*?)\s.*?-{10,}/gis)
     )?.[0]?.groups?.url
-    await fs.rm(e, { recursive: true, force: true })
+      */
 
     while (
       !(await fileExists(continuePath1)) &&
